2ea3e0c7e032ee7c9f0b48927d4e62f0fb3177e4
Мирослав Дорошенко <myroslavdoroshenko@MacBook-Pro-Miroslav.local>
2023-05-15 10:21:41 +0300

    ++

--- cypress/e2e/Test.cy.js
+++ cypress/e2e/Test.cy.js
@@ -1,10 +1,26 @@
-describe("Log i", () => {
-    it("Log i", () => {
-        cy.visit("https://moniheal.com");
+describe("Log in", () => {
+    it("Log in", () => {
+        cy.visit("https://moniheal.com"); // Перехід на сайт
         
-        cy.get('.footer-main__block--first > .common_list > :nth-child(3) > .common_list-link').scrollIntoView().click();
-        
-        cy.go("back");
+        cy.viewport(1920, 1080)   
+
+        cy.get(':nth-child(1) > .news-nav__link')  // Кнопка Знайти лікаря
+        .wait(1000)
+        .click()
+
+        cy.get("#name")   // Find a doctor
+        .type("Пришко")
+
+        cy.get('.find-filter__button > .button')
+        .click() // Prushko
+
+        cy.get('[data-id="6"] > .expert-preview__hide > .expert-preview__hide-footer > .button_medium')
+        .click()
+
+        cy.get('[data-title="Консультація для Пацієнтів"] > .btn-wrap > .button_regular')
+        .click()
+
+
 
 
    
